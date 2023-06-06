04e2d5f5588e89aea1318602e2a0c083b24f0ad3
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-06-06 19:39:46 -0300

    .

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import Formulario from "./components/Formulario"
+import Lista from "./components/Lista"
+
+
+function App() {
+
+
+  return (
+    <>
+  hello world
+  <Formulario/>
+  <Lista/>
+    </>
+  )
+}
+
+export default App
